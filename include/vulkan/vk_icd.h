d24c08bcedb3aa935b6cc832cee500497d9bf113
Joe Brown <grim210@users.noreply.github.com>
2017-07-01 12:42:15 -0500

    headers: Enforce C-style comments
    When compiling with -std=c90, GCC complains about C++ style comments.
    These changes do no affect readability of the header files and
    ensures that the headers can be included in any C90-conformant code.

--- include/vulkan/vk_icd.h
+++ include/vulkan/vk_icd.h
@@ -1,7 +1,7 @@
-//
-// File: vk_icd.h
-//
 /*
+ * File: vk_icd.h
+ *
+ *
  * Copyright (c) 2015-2016 The Khronos Group Inc.
  * Copyright (c) 2015-2016 Valve Corporation
  * Copyright (c) 2015-2016 LunarG, Inc.
@@ -26,28 +26,30 @@
 #include "vulkan.h"
 #include <stdbool.h>
 
-// Loader-ICD version negotiation API.  Versions add the following features:
-//   Version 0 - Initial.  Doesn't support vk_icdGetInstanceProcAddr
-//               or vk_icdNegotiateLoaderICDInterfaceVersion.
-//   Version 1 - Add support for vk_icdGetInstanceProcAddr.
-//   Version 2 - Add Loader/ICD Interface version negotiation
-//               via vk_icdNegotiateLoaderICDInterfaceVersion.
-//   Version 3 - Add ICD creation/destruction of KHR_surface objects.
-//   Version 4 - Add unknown physical device extension qyering via
-//               vk_icdGetPhysicalDeviceProcAddr.
-//   Version 5 - Tells ICDs that the loader is now paying attention to the
-//               application version of Vulkan passed into the ApplicationInfo
-//               structure during vkCreateInstance.  This will tell the ICD
-//               that if the loader is older, it should automatically fail a
-//               call for any API version > 1.0.  Otherwise, the loader will
-//               manually determine if it can support the expected version.
+/* Loader-ICD version negotiation API.  Versions add the following features:
+ *   Version 0 - Initial.  Doesn't support vk_icdGetInstanceProcAddr
+ *               or vk_icdNegotiateLoaderICDInterfaceVersion.
+ *   Version 1 - Add support for vk_icdGetInstanceProcAddr.
+ *   Version 2 - Add Loader/ICD Interface version negotiation
+ *               via vk_icdNegotiateLoaderICDInterfaceVersion.
+ *   Version 3 - Add ICD creation/destruction of KHR_surface objects.
+ *   Version 4 - Add unknown physical device extension qyering via
+ *               vk_icdGetPhysicalDeviceProcAddr.
+ *   Version 5 - Tells ICDs that the loader is now paying attention to the
+ *               application version of Vulkan passed into the ApplicationInfo
+ *               structure during vkCreateInstance.  This will tell the ICD
+ *               that if the loader is older, it should automatically fail a
+ *               call for any API version > 1.0.  Otherwise, the loader will
+ *               manually determine if it can support the expected version.
+ */
 #define CURRENT_LOADER_ICD_INTERFACE_VERSION 5
 #define MIN_SUPPORTED_LOADER_ICD_INTERFACE_VERSION 0
 #define MIN_PHYS_DEV_EXTENSION_ICD_INTERFACE_VERSION 4
 typedef VkResult (VKAPI_PTR *PFN_vkNegotiateLoaderICDInterfaceVersion)(uint32_t *pVersion);
 
-// This is defined in vk_layer.h which will be found by the loader, but if an ICD is building against this
-// flie directly, it won't be found.
+/* This is defined in vk_layer.h which will be found by the loader, but if an ICD is building against this
+ * flie directly, it won't be found.
+ */
 #ifndef PFN_GetPhysicalDeviceProcAddr
 typedef PFN_vkVoidFunction (VKAPI_PTR *PFN_GetPhysicalDeviceProcAddr)(VkInstance instance, const char* pName);
 #endif
@@ -98,7 +100,7 @@ typedef struct {
     MirConnection *connection;
     MirSurface *mirSurface;
 } VkIcdSurfaceMir;
-#endif // VK_USE_PLATFORM_MIR_KHR
+#endif /* VK_USE_PLATFORM_MIR_KHR */
 
 #ifdef VK_USE_PLATFORM_WAYLAND_KHR
 typedef struct {
@@ -106,7 +108,7 @@ typedef struct {
     struct wl_display *display;
     struct wl_surface *surface;
 } VkIcdSurfaceWayland;
-#endif // VK_USE_PLATFORM_WAYLAND_KHR
+#endif /* VK_USE_PLATFORM_WAYLAND_KHR */
 
 #ifdef VK_USE_PLATFORM_WIN32_KHR
 typedef struct {
@@ -114,7 +116,7 @@ typedef struct {
     HINSTANCE hinstance;
     HWND hwnd;
 } VkIcdSurfaceWin32;
-#endif // VK_USE_PLATFORM_WIN32_KHR
+#endif /* VK_USE_PLATFORM_WIN32_KHR */
 
 #ifdef VK_USE_PLATFORM_XCB_KHR
 typedef struct {
@@ -122,7 +124,7 @@ typedef struct {
     xcb_connection_t *connection;
     xcb_window_t window;
 } VkIcdSurfaceXcb;
-#endif // VK_USE_PLATFORM_XCB_KHR
+#endif /* VK_USE_PLATFORM_XCB_KHR */
 
 #ifdef VK_USE_PLATFORM_XLIB_KHR
 typedef struct {
@@ -130,13 +132,13 @@ typedef struct {
     Display *dpy;
     Window window;
 } VkIcdSurfaceXlib;
-#endif // VK_USE_PLATFORM_XLIB_KHR
+#endif /* VK_USE_PLATFORM_XLIB_KHR */
 
 #ifdef VK_USE_PLATFORM_ANDROID_KHR
 typedef struct {
     ANativeWindow* window;
 } VkIcdSurfaceAndroid;
-#endif //VK_USE_PLATFORM_ANDROID_KHR
+#endif /* VK_USE_PLATFORM_ANDROID_KHR */
 
 typedef struct {
     VkIcdSurfaceBase base;
@@ -149,4 +151,4 @@ typedef struct {
     VkExtent2D imageExtent;
 } VkIcdSurfaceDisplay;
 
-#endif // VKICD_H
+#endif /* VKICD_H */
